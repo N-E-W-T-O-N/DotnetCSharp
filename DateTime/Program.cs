51600de5d4911b20a6bbe865f0751c6638b24a42
newton.mallick <newton.mallick@celebaltech.com>
2024-02-01 10:14:22 +0000

    Added a Program to understand DateTime

---
+++ DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program
+{
+    public class Employee
+    {
+        public bool IsBirthDay(DateTime dt,int n=30)
+        {
+            var x = dt.DayOfYear;// .CompareTo(DateTime.Now);
+            var y = DateTime.Now.DayOfYear;
+            if (x < y) return false; //Date already went
+            else if (x -y <= n) return true; //Lease Within n dates
+            else return false; //Acced n days
+        }
+    }
+
+    public class P
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new ();
+            bool t =employee.IsBirthDay(new DateTime(1978,02,01)); //Lie
+            Console.WriteLine(t);
+            t = employee.IsBirthDay(new DateTime(1978, 07, 01)); //Exceed
+            Console.WriteLine(t);
+            t = employee.IsBirthDay(new DateTime(1978, 01, 01),5); //Already Passed
+            Console.WriteLine(t);
+        }
+    }
+}
