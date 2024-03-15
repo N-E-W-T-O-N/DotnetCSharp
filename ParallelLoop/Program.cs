cd994d67dc8a0d2d35b3dfb8e0e1f449adb4838f
newton.mallick <newton.mallick@celebaltech.com>
2024-03-15 06:41:32 +0000

    Added ParallelLoop example from Task Parallel Library

---
+++ ParallelLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        //Choose a large number 
+        int[] numbers = Enumerable.Range(1, 100).ToArray(); 
+        int[] squares = new int[numbers.Length];
+
+        // Parallel loop to calculate squares
+        Parallel.For(0, numbers.Length, i =>
+        {
+            squares[i] = numbers[i] * numbers[i];
+            Console.WriteLine($"Square of {numbers[i]} is {squares[i]}");
+        });
+
+        Console.WriteLine("All squares calculated.");
+    }
+}
