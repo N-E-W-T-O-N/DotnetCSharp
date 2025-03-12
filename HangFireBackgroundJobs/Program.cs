43b67f2e2a26c418e340ebea9e127a526b66b2f8
NEWTON MALLICK <NEW99MAL@GMAIL.COM>
2025-03-12 08:17:28 +0000

    Added HangFire

---
+++ HangFireBackgroundJobs/Program.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHangfire(conf =>
+
+    conf.UseSimpleAssemblyNameTypeSerializer()
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseInMemoryStorage()
+);
+builder.Services.AddHangfireServer(srv => srv.SchedulePollingInterval = TimeSpan.FromSeconds(2));
+var app = builder.Build();
+app.UseHangfireDashboard();
+app.MapGet("/job", (IBackgroundJobClient jobClient,IRecurringJobManager jobManager) =>
+{
+    //TO ENQUEUE
+    //jobClient.Enqueue(() => Console.WriteLine("Do it"));
+
+    //Run AFTER SomeTime
+    jobClient.Schedule(() => Console.WriteLine("Do it 3 Times"),TimeSpan.FromSeconds(3));
+
+    jobManager.AddOrUpdate("every5seconds",() => Console.WriteLine("Do it 5 Times"), "*/5 * * * *");
+    return Results.Ok("");
+});
+app.Run();
